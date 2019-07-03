f857386f7f35f05343a93f63e929d51d3c0bcd78
Scott Brenner <brenner.scott.980@icloud.com>
2019-07-03 15:43:56 +0900

    DateComponents

--- Sources/SBSwifterSwift/Foundation/DateComponentsExtensions.swift
+++ Sources/SBSwifterSwift/Foundation/DateComponentsExtensions.swift
@@ -95,7 +95,7 @@ extension DateComponents
         return false
     }
     
-    /// Flip the sign of all values that are not `NSDateComponentUndefined`.
+    /// Negates all values that are not `NSDateComponentUndefined`.
     public static prefix func - (rhs: DateComponents) -> DateComponents {
         var components = DateComponents()
         components.era = rhs.era.map(-)
@@ -112,6 +112,79 @@ extension DateComponents
         components.weekOfMonth = rhs.weekOfMonth.map(-)
         components.weekOfYear = rhs.weekOfYear.map(-)
         components.yearForWeekOfYear = rhs.yearForWeekOfYear.map(-)
+        return components
+    }
+    
+    /// Adds two DateComponents and returns their combined individual components.
+    public static func + (lhs: DateComponents, rhs: DateComponents) -> DateComponents {
+        combine(lhs, rhs: rhs, transform: +)
+    }
+    
+    /// Subtracts two DateComponents and returns the relative difference between them.
+    public static func - (lhs: DateComponents, rhs: DateComponents) -> DateComponents {
+        lhs + (-rhs)
+    }
+    
+    /// Subscription support for `DateComponents` instances.
+    /// ````
+    /// let components = DateComponents(day: 5)
+    /// components[.day]
+    /// // 5
+    /// ````
+    /// - Important: This does not take into account any built-in errors, `Int.max` returned instead of `nil`.
+    /// - Parameter component: Component to get.
+    public subscript(component: Calendar.Component) -> Int? {
+        switch component {
+        case .era:                  return era
+        case .year:                 return year
+        case .month:                return month
+        case .day:                  return day
+        case .hour:                 return hour
+        case .minute:               return minute
+        case .second:               return second
+        case .weekday:              return weekday
+        case .weekdayOrdinal:       return weekdayOrdinal
+        case .quarter:              return quarter
+        case .weekOfMonth:          return weekOfMonth
+        case .weekOfYear:           return weekOfYear
+        case .yearForWeekOfYear:    return yearForWeekOfYear
+        case .nanosecond:           return nanosecond
+        case .calendar, .timeZone:  return nil
+        @unknown default:           return nil
+        }
+    }
+    
+    
+    // MARK: - Helper methods
+    
+    /// Applies the `transform` to both `T` values provided, defaulting either of them if `nil`.
+    /// - Parameter a: Optional value.
+    /// - Parameter b: Optional value.
+    /// - Parameter default: The default value to use in the event `a` or `b` are `nil`.
+    /// - Parameter transform: The transform to use.
+    /// - Returns: The transformed value. If both `T` values are `nil`, returns `nil`.
+    private static func bimap<T>(_ a: T?, _ b: T?, default: T, _ transform: (T, T) -> T) -> T? {
+        if a == nil && b == nil { return nil }
+        return transform(a ?? `default`, b ?? `default`)
+    }
+    
+    /// Combines two date components using the provided `transform` on all values within the components that are not `NSDateComponentUndefined`.
+    private static func combine(_ lhs: DateComponents, rhs: DateComponents, transform: (Int, Int) -> Int) -> DateComponents {
+        var components = DateComponents()
+        components.era = bimap(lhs.era, rhs.era, default: 0, transform)
+        components.year = bimap(lhs.year, rhs.year, default: 0, transform)
+        components.month = bimap(lhs.month, rhs.month, default: 0, transform)
+        components.day = bimap(lhs.day, rhs.day, default: 0, transform)
+        components.hour = bimap(lhs.hour, rhs.hour, default: 0, transform)
+        components.minute = bimap(lhs.minute, rhs.minute, default: 0, transform)
+        components.second = bimap(lhs.second, rhs.second, default: 0, transform)
+        components.nanosecond = bimap(lhs.nanosecond, rhs.nanosecond, default: 0, transform)
+        components.weekday = bimap(lhs.weekday, rhs.weekday, default: 0, transform)
+        components.weekdayOrdinal = bimap(lhs.weekdayOrdinal, rhs.weekdayOrdinal, default: 0, transform)
+        components.quarter = bimap(lhs.quarter, rhs.quarter, default: 0, transform)
+        components.weekOfMonth = bimap(lhs.weekOfMonth, rhs.weekOfMonth, default: 0, transform)
+        components.weekOfYear = bimap(lhs.weekOfYear, rhs.weekOfYear, default: 0, transform)
+        components.yearForWeekOfYear = bimap(lhs.yearForWeekOfYear, rhs.yearForWeekOfYear, default: 0, transform)
         return components
     }
 }
--- Tests/SBSwifterSwiftTests/Foundation/DateComponentsExtensionsTests.swift
+++ Tests/SBSwifterSwiftTests/Foundation/DateComponentsExtensionsTests.swift
@@ -48,6 +48,49 @@ final class DateComponentsExtensionsTests: XCTestCase {
         XCTAssertEqual(-dateComponents1, dateComponents2)
     }
     
+    func testAddition() {
+        XCTAssertEqual(DateComponents(hour: 4) + DateComponents(minute: 15), DateComponents(hour: 4, minute: 15))
+    }
+    
+    func testSubtraction() {
+        XCTAssertEqual(DateComponents(hour: 4) - DateComponents(minute: 15), DateComponents(hour: 4, minute: -15))
+    }
+    
+    func testSubscription() {
+        let components = DateComponents(calendar: .current,
+                                        timeZone: .current,
+                                        era: 1,
+                                        year: 1,
+                                        month: 1,
+                                        day: 1,
+                                        hour: 1,
+                                        minute: 1,
+                                        second: 1,
+                                        nanosecond: 1,
+                                        weekday: 1,
+                                        weekdayOrdinal: 1,
+                                        quarter: 1,
+                                        weekOfMonth: 1,
+                                        weekOfYear: 1,
+                                        yearForWeekOfYear: 1)
+        XCTAssertEqual(components[.era], 1)
+        XCTAssertEqual(components[.year], 1)
+        XCTAssertEqual(components[.month], 1)
+        XCTAssertEqual(components[.day], 1)
+        XCTAssertEqual(components[.hour], 1)
+        XCTAssertEqual(components[.minute], 1)
+        XCTAssertEqual(components[.second], 1)
+        XCTAssertEqual(components[.weekday], 1)
+        XCTAssertEqual(components[.weekdayOrdinal], 1)
+        XCTAssertEqual(components[.quarter], 1)
+        XCTAssertEqual(components[.weekOfMonth], 1)
+        XCTAssertEqual(components[.weekOfYear], 1)
+        XCTAssertEqual(components[.yearForWeekOfYear], 1)
+        XCTAssertEqual(components[.nanosecond], 1)
+        XCTAssertNil(components[.calendar])
+        XCTAssertNil(components[.timeZone])
+    }
+    
     func testComparable() {
         XCTAssertTrue(DateComponents(second: 10) > DateComponents(nanosecond: 10))
     }
