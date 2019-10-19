845ff1c7e6b4701f74d11881b6f9db1fb2fdd28d
Scott Brenner <brenner.scott.980@icloud.com>
2019-10-19 10:32:28 +0900

    Published

--- Sources/SBSwifterSwift/Property Wrappers/Published.swift
+++ Sources/SBSwifterSwift/Property Wrappers/Published.swift
@@ -7,12 +7,12 @@ private var cancellables = [String : AnyCancellable]()
 @available(iOS 13.0, *)
 extension Published {
     
-    public init(wrappedValue value: Value, key: String) {
+    public init(wrappedValue value: Value, key: String, defaults: UserDefaults = .standard) {
         
-        let value = UserDefaults.standard.object(forKey: key) as? Value ?? value
+        let value = defaults.object(forKey: key) as? Value ?? value
         self.init(initialValue: value)
-        cancellables[key] = projectedValue.sink { value in
-            UserDefaults.standard.set(value, forKey: key)
+        cancellables[key] = projectedValue.sink { val in
+            defaults.set(val, forKey: key)
         }
     }
 }
