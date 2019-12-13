20b72be8303edaf195fba13431fe9b6b69c54f41
Scott Brenner <brenner.scott.980@icloud.com>
2019-12-13 12:16:04 +0900

    restricted platform support to iOS v12 and newer

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SBSwifterSwift",
+    platforms: [
+        .iOS(.v12), .tvOS(.v12), .watchOS(.v6), .macOS(.v10_14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
