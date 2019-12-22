274a4a82382443b19a4c7828ceb0e859eeb8ae16
Scott Brenner <brenner.scott.980@icloud.com>
2019-12-22 13:30:21 +0900

    deleted

--- Sources/SBSwifterSwift/UIKit/UIEdgeInsetsExtensions.swift
+++ Sources/SBSwifterSwift/UIKit/UIEdgeInsetsExtensions.swift
@@ -1,30 +1,30 @@
-#if canImport(UIKit)
-import UIKit
-
-extension UIEdgeInsets {
-    
-    /// The inset to use for a scroll view in response to
-    /// - Parameters:
-    ///   - notification: The notification that dictates the return value. Only notifications named `UIResponder.keyboardWillChangeFrameNotification` will return a nonzero inset.
-    ///   - view: The view used to calculate the keyboard height. Normally the scroll view's superview.
-    
-    public static func insetForKeyboardNotification(
-        _ notification: Notification,
-        in view: UIView) -> UIEdgeInsets {
-        
-        switch notification.name {
-        case UIResponder.keyboardWillChangeFrameNotification:
-            guard let userInfo = notification.userInfo,
-                let keyboardFrameEndValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
-                    return .zero
-            }
-            let keyboardFrameEnd = view.convert(keyboardFrameEndValue.cgRectValue, from: view.window)
-            let height = keyboardFrameEnd.height - view.safeAreaInsets.bottom
-            return .init(top: 0, left: 0, bottom: height, right: 0)
-            
-        default:
-            return .zero
-        }
-    }
-}
-#endif
+//#if canImport(UIKit)
+//import UIKit
+//
+//extension UIEdgeInsets {
+//    
+//    /// The inset to use for a scroll view in response to
+//    /// - Parameters:
+//    ///   - notification: The notification that dictates the return value. Only notifications named `UIResponder.keyboardWillChangeFrameNotification` will return a nonzero inset.
+//    ///   - view: The view used to calculate the keyboard height. Normally the scroll view's superview.
+//    
+//    public static func insetForKeyboardNotification(
+//        _ notification: Notification,
+//        in view: UIView) -> UIEdgeInsets {
+//        
+//        switch notification.name {
+//        case UIResponder.keyboardWillChangeFrameNotification:
+//            guard let userInfo = notification.userInfo,
+//                let keyboardFrameEndValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+//                    return .zero
+//            }
+//            let keyboardFrameEnd = view.convert(keyboardFrameEndValue.cgRectValue, from: view.window)
+//            let height = keyboardFrameEnd.height - view.safeAreaInsets.bottom
+//            return .init(top: 0, left: 0, bottom: height, right: 0)
+//            
+//        default:
+//            return .zero
+//        }
+//    }
+//}
+//#endif
