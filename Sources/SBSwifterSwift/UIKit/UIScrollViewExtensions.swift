aec7fe2ab6d23250d5457347deb7e6c814b4d2f5
Scott Brenner <brenner.scott.980@icloud.com>
2019-12-22 15:01:28 +0900

    animation fix

--- Sources/SBSwifterSwift/UIKit/UIScrollViewExtensions.swift
+++ Sources/SBSwifterSwift/UIKit/UIScrollViewExtensions.swift
@@ -13,13 +13,13 @@ extension UIScrollView {
     public final func adjustForKeyboardNotification(_ notification: UIResponder.KeyboardNotification) {
 
         let bottom = notification.event == .willShow ? notification.frameEnd.height - (superview?.safeAreaInsets.bottom ?? 0) : 0
-
-        UIView.animate(withDuration: notification.animationDuration) { [weak self] in
-
+        
+        let animator = UIViewPropertyAnimator(duration: notification.animationDuration, curve: notification.animationCurve) { [weak self] in
             guard let self = self else { return }
             self.contentInset.bottom = bottom
             self.verticalScrollIndicatorInsets.bottom = bottom
         }
+        animator.startAnimation()
     }
 }
 
