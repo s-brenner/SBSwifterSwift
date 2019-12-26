61900e40730863392229e3f3cdea9ab14cfd3cd7
Scott Brenner <brenner.scott.980@icloud.com>
2019-12-26 18:12:27 +0900

    UIImageView

---
+++ Sources/SBSwifterSwift/UIKit/UIImageViewExtensions.swift
@@ -0,0 +1,13 @@
+#if canImport(UIKit)
+import UIKit
+
+extension UIImageView {
+    
+    public func setImage(to image: UIImage?, animate: Bool = true) {
+        
+        fadeTransition(duration: animate ? 0.25 : 0)
+        self.image = image
+    }
+}
+
+#endif
