98a6a71cb87702d0e50366f85b2dbbbefad5fba0
Scott Brenner <brenner.scott.980@icloud.com>
2019-12-29 15:47:01 +0900

    indexPath Identifier

---
+++ Sources/SBSwifterSwift/Foundation/IndexPathExtensions.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public extension IndexPath {
+    
+    typealias Identifier = NSString
+    
+    var identifier: Identifier { "\(row),\(section)" as Identifier }
+    
+    init?(identifier: Identifier) {
+        
+        let integers = (identifier as String).split(separator: ",").compactMap() { Int($0) }
+        
+        guard integers.count == 2 else { return nil }
+        
+        self.init(row: integers[0], section: integers[1])
+    }
+}
