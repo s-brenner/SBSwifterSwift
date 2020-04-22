71ed17373e53b28a10d9809595a9f07fc8164ab2
Scott Brenner <brenner.scott.980@icloud.com>
2020-04-22 10:07:34 +0900

    PropertyList

---
+++ Sources/SBSwifterSwift/PropertyList.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public enum PropertyList {
+    
+    /// Returns the dictionary for the .plist file identified by the specified name.
+    /// - Author: Scott Brenner | SBSwifterSwift
+    /// - Parameter name: The name of the resource .plist file.
+    /// - Parameter bundle: The bundle in which the file is contained.
+    /// - Returns: The dictionary for the .plist file or an empty dictionary if the resource could not be parsed.
+    public static func named(_ name: String, inBundle bundle: Bundle = .main) -> [String: Any] {
+        
+        do {
+            guard let path = bundle.path(forResource: name, ofType: "plist") else { return [:] }
+            guard let xml = FileManager.default.contents(atPath: path) else { return [:] }
+            var format = PropertyListSerialization.PropertyListFormat.xml
+            let list = try PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: &format)
+            return list as? [String: Any] ?? [:]
+        }
+        catch let error {
+            NSLog(error.localizedDescription)
+            return [:]
+        }
+    }
+    
+    public enum Info {
+        
+        public static let list = PropertyList.named("Info")
+        
+        public static var name: String { list["CFBundleDisplayName"] as? String ?? "" }
+        
+        public static var version: String { list["CFBundleShortVersionString"] as? String ?? "" }
+        
+        public static var build: String { list["CFBundleVersion"] as? String ?? "" }
+    }
+}
