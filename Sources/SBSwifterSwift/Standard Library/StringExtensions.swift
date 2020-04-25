2b71d8a8feadd754c90ed4b7616de2f6394610e0
Scott Brenner <brenner.scott.980@icloud.com>
2020-04-25 14:51:37 +0900

    String extensions

--- Sources/SBSwifterSwift/Standard Library/StringExtensions.swift
+++ Sources/SBSwifterSwift/Standard Library/StringExtensions.swift
@@ -2,7 +2,8 @@ import Foundation
 
 extension String {
     
-    /// CamelCase of string.
+    /// Camel cased string.
+    /// - Author: Scott Brenner | SBSwifterSwift
     /// ````
     /// "sOme vAriable naMe".camelCased
     /// // "someVariableName"
@@ -14,14 +15,80 @@ extension String {
         return first + rest
     }
     
+    /// Returns the Base-64 encoding of the receiver.
+    /// - Author: Scott Brenner | SBSwifterSwift
+    /// - Parameter encoding: The text encoding to use.
+    /// - Returns: The Base-64 encoding of the receiver.
     public func base64Encoded(using encoding: Encoding = .utf8) -> String? {
         
         data(using: encoding)?.base64EncodedString()
     }
     
+    /// Returns the Base-64 decoding of the receiver.
+    /// - Author: Scott Brenner | SBSwifterSwift
+    /// - Parameter encoding: The text encoding to use.
+    /// - Returns: The Base-64 decoding of the receiver.
     public func base64Decoded(using encoding: Encoding = .utf8) -> String? {
         
         guard let data = Data(base64Encoded: self) else { return nil }
         return String(data: data, encoding: encoding)
+    }
+    
+    /// A random string of a prescribed length including only the allowed characters.
+    /// - Author: Scott Brenner | SBSwifterSwift
+    /// - Parameter length: The number of characters in the output.
+    /// - Parameter allowed: The characters allowed in the output.
+    public init(
+        randomWithLength length: Int,
+        allowedCharacters allowed: AllowedCharacters = .alphaNumeric) {
+        
+        self = String((0..<length).map { _ in allowed.characters.randomElement()! })
+    }
+    
+    /// Returns a Boolean value that indicates whether the receiver contains only the allowed characters.
+    /// - Author: Scott Brenner | SBSwifterSwift
+    /// - Complexity: O(n), where n is the length of the receiver.
+    /// - Parameter allowed: The characters allowed in the receiver.
+    /// - Returns: `true` if receiver contains only the allowed characters; otherwise, `false`.
+    public func contains(only allowed: AllowedCharacters) -> Bool {
+        
+        let characters = Set(allowed.characters)
+        for element in self {
+            guard characters.contains(element) else { return false }
+        }
+        return true
+    }
+    
+    public enum AllowedCharacters: CustomStringConvertible {
+        
+        /// Allow all numbers from 0 to 9.
+        case numeric
+        
+        /// Allow all alphabetic characters ignoring case.
+        case alphabetic
+        
+        /// Allow both numbers and alphabetic characters ignoring case.
+        case alphaNumeric
+        
+        /// Allow all characters appearing within the specified String.
+        case allCharactersIn(String)
+        
+        public var characters: [Character] { Array(description) }
+        
+        public var description: String {
+            switch self {
+            case .numeric:
+                return "0123456789"
+                
+            case .alphabetic:
+                return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                
+            case .alphaNumeric:
+                return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+                
+            case .allCharactersIn(let allowed):
+                return allowed
+            }
+        }
     }
 }
--- Tests/SBSwifterSwiftTests/Standard Library/StringExtensionsTests.swift
+++ Tests/SBSwifterSwiftTests/Standard Library/StringExtensionsTests.swift
@@ -4,6 +4,31 @@ import XCTest
 final class StringExtensionsTests: XCTestCase {
     
     func testCamelCased() {
+        
         XCTAssertEqual("SoMe vAriable naMe".camelCased, "someVariableName")
+    }
+    
+    func testBase64() {
+        
+        let string = "test:secret"
+        let base64 = string.base64Encoded()
+        let random = String(randomWithLength: 100)
+        
+        XCTAssertEqual(base64, "dGVzdDpzZWNyZXQ=")
+        XCTAssertEqual(base64?.base64Decoded(), string)
+        XCTAssertEqual(random.base64Encoded()?.base64Decoded(), random)
+    }
+    
+    func testRandom() {
+        
+        let allowed: [String.AllowedCharacters] = [.numeric, .alphabetic, .alphaNumeric]
+        allowed.forEach {
+            
+            let length = 100
+            let random = String(randomWithLength: 100, allowedCharacters: $0)
+            
+            XCTAssertEqual(random.count, length)
+            XCTAssertTrue(random.contains(only: $0))
+        }
     }
 }
