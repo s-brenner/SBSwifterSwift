363ad6f2ba8d92dfbe1d1cc5103c9de067d63679
Scott Brenner <brenner.scott.980@icloud.com>
2020-04-25 14:51:27 +0900

    URLExtensions

--- Sources/SBSwifterSwift/Foundation/URLRequestExtensions.swift
+++ Sources/SBSwifterSwift/Foundation/URLRequestExtensions.swift
@@ -18,6 +18,8 @@ extension URLRequest {
         
         private var headers = [URLRequest.HTTPHeaderField]()
         
+        private var body: Data?
+        
         public init() {}
     }
 }
@@ -84,6 +86,17 @@ extension URLRequest.Builder {
         return self
     }
     
+    public func withBody(_ body: Data?) -> Self {
+        
+        self.body = body
+        return self
+    }
+    
+    public func withJSONBody<Value: Encodable>(_ value: Value, encodedWith encoder: JSONEncoder = .init()) -> Self {
+        
+        self.withBody(try? encoder.encode(value))
+    }
+    
     public func build() -> URLRequest {
         
         switch compose() {
@@ -102,13 +115,14 @@ extension URLRequest.Builder {
         components.host = host
         components.port = port
         components.path = path
-        components.queryItems = queryItems
+        components.queryItems = queryItems.isEmpty ? nil : queryItems
         
         guard let url = components.url else { return .failure(.invalidURL) }
         
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
         headers.forEach { request.setHTTPHeaderField(to: $0) }
+        request.httpBody = body
         
         return .success(request)
     }
@@ -120,24 +134,83 @@ extension URLRequest {
     public enum MIMEType: CustomStringConvertible {
         
         case application(ApplicationSubtype)
+        case image(ImageSubtype)
+        case multipart(MultipartSubtype)
+        case text(TextSubtype)
         
         private var type: String {
             switch self {
-            case .application: return "application"
+            case .application:
+                return "application"
+                
+            case .image:
+                return "image"
+                
+            case .multipart:
+                return "multipart"
+                
+            case .text:
+                return "text"
             }
         }
         
         public var description: String {
-            switch self {
-            case .application(let subtype): return "\(type)/\(subtype.rawValue)"
+            var subtype: String {
+                switch self {
+                case .application(let sub):
+                    return sub.description
+                    
+                case .image(let sub):
+                    return sub.description
+                    
+                case .multipart(let sub):
+                    return sub.description
+                    
+                case .text(let sub):
+                    return sub.description
+                }
             }
+            return "\(type)/\(subtype)"
         }
         
-        public enum ApplicationSubtype: String, CustomStringConvertible {
+        public enum ApplicationSubtype: String, MIMESubtype {
+            case atom_xml = "atom+xml"
+            case ecmascript
             case json
+            case javascript
+            case octet_stream = "octet-stream"
+            case ogg
+            case pdf
+            case postscript
+            case rdf_xml = "rdf+xml"
+            case rss_xml = "rss+xml"
+            case soap_xml = "soap+xml"
+            case font_woff = "font-woff"
+            case x_yaml = "x-yaml"
+            case xhtml_xml = "xhtml+xml"
+            case xml
+            case xml_dtd = "xml-dtd"
+            case xop_xml = "xop+xml"
+            case zip
+            case gzip
+            case graphql
             case x_www_form_urlencoded = "x-www-form-urlencoded"
-            
-            public var description: String { rawValue }
+        }
+        
+        public enum ImageSubtype: String, MIMESubtype {
+            case gif, jpeg, pjpeg, png
+            case svg_xml = "svg+xml"
+            case tiff
+        }
+        
+        public enum MultipartSubtype: String, MIMESubtype {
+            case mixed, alternative, related
+            case form_data = "form-data"
+            case signed, encrypted
+        }
+        
+        public enum TextSubtype: String, MIMESubtype {
+            case cmd, css, csv, html, plain, vcard, xml
         }
     }
     
@@ -145,21 +218,34 @@ extension URLRequest {
         case accept([MIMEType])
         case authorization(Authorization)
         
-        public var field: String {
+        public var key: Key {
             switch self {
-            case .accept: return "Accept"
-            case .authorization: return "Authorization"
+            case .accept:
+                return .accept
+                
+            case .authorization:
+                return .authorization
             }
         }
         
         public var value: String {
             switch self {
-            case .accept(let mimes): return mimes.map { $0.description }.joined(separator: ",")
-            case .authorization(let auth): return auth.value
+            case .accept(let mimes):
+                return mimes.map { $0.description }.joined(separator: ",")
+                
+            case .authorization(let auth):
+                return auth.value
             }
         }
         
-        public var description: String { "\(field): \(value)" }
+        public var description: String { "\(key.description): \(value)" }
+        
+        public enum Key: String, CustomStringConvertible {
+            case accept
+            case authorization
+            
+            public var description: String { rawValue.capitalized }
+        }
         
         public enum Authorization {
             case basic(username: String, password: String)
@@ -177,35 +263,21 @@ extension URLRequest {
         }
     }
     
-//    public func addValue(_ value: String, forHTTPHeaderField field: HTTPHeaderField) {
-//
-//        addValue(value, forHTTPHeaderField: field.rawValue)
-//    }
-//
     public mutating func setHTTPHeaderField(to field: HTTPHeaderField) {
 
-        setValue(field.value, forHTTPHeaderField: field.field)
+        setValue(field.value, forHTTPHeaderField: field.key.description)
     }
     
-    public func value(forHTTPHeaderField field: HTTPHeaderField) -> String? {
+    public func value(forHTTPHeaderField key: HTTPHeaderField.Key) -> String? {
         
-        value(forHTTPHeaderField: field.field)
+        value(forHTTPHeaderField: key.description)
     }
 }
 
 
-class XXX {
+public protocol MIMESubtype: RawRepresentable, CustomStringConvertible {}
+
+public extension MIMESubtype where Self.RawValue == String {
     
-    func x() {
-        
-        let request = URLRequest.Builder()
-            .withScheme(.http)
-            .withHost("127.0.0.1")
-            .withPort(8080)
-            .withPath("/v1/users/login")
-            .withMethod(.get)
-            .withHeader(.accept([.application(.json)]))
-            .withHeader(.authorization(.basic(username: "brenner.scott@gmail.com", password: "secret980")))
-            .build()
-    }
+    var description: String { rawValue }
 }
---
+++ Sources/SBSwifterSwift/Foundation/URLSessionExtensions.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+extension URLSession {
+    
+    public func dataTask(
+        with url: URL,
+        completionHandler: @escaping (_ result: Result<Data, Error>, _ response: URLResponse?) -> Void
+    ) -> URLSessionDataTask {
+        
+        dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                completionHandler(.failure(error), response)
+            }
+            else {
+                completionHandler(.success(data ?? Data()), response)
+            }
+        }
+    }
+    
+    public func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping (_ result: Result<Data, Error>, _ response: URLResponse?) -> Void
+    ) -> URLSessionDataTask {
+        
+        dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                completionHandler(.failure(error), response)
+            }
+            else {
+                completionHandler(.success(data ?? Data()), response)
+            }
+        }
+    }
+    
+    
+    /// Creates a task that retrieves the contents of a URL based on the specified URL request object, decodes the JSON payload, and calls a handler upon completion.
+    /// - Author: Scott Brenner | SBSwifterSwift
+    /// - Parameter request: A URL request object that provides the URL, cache policy, request type, body data or body stream, and so on.
+    /// - Parameter type: The type of object into which the decoder will decode the server data.
+    /// - Parameter decoder: The decoder to use.
+    /// - Parameter completionHandler: The completion handler to call when the load request is complete. This handler is executed on the delegate queue.
+    /// - Parameter result: The decoded object returned by the server or an error object that indicates why the request failed.
+    /// - Parameter response: An object that provides response metadata, such as HTTP headers and status code.
+    /// - Returns: The new session data task.
+    public func dataTask<T: Decodable>(
+        with request: URLRequest,
+        decodedAs type: T.Type,
+        usingDecoder decoder: JSONDecoder = .init(),
+        completionHandler: @escaping (_ result: Result<T, Error>, _ response: URLResponse?) -> Void
+    ) -> URLSessionDataTask {
+        
+        dataTask(with: request) { result, response in
+            
+            let transformed = result.flatMap { data in
+                Result { try decoder.decode(T.self, from: data) }
+            }
+            
+            switch transformed {
+            case .success(let item):
+                completionHandler(.success(item), response)
+                
+            case .failure(let error):
+                completionHandler(.failure(error), response)
+            }
+        }
+    }
+}
