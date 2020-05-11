768cc0ec747e0ab8e9a006674f3a44124f877f24
Scott Brenner <brenner.scott.980@icloud.com>
2020-05-11 22:09:28 +0900

    back button

--- Sources/SBSwifterSwift/UIKit/UIBarButtonItemExtensions.swift
+++ Sources/SBSwifterSwift/UIKit/UIBarButtonItemExtensions.swift
@@ -3,6 +3,9 @@ import UIKit
 
 public extension UIBarButtonItem {
     
+    /// A back button whose title is simply "Back".
+    static let back = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+    
     static let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
 }
 #endif
