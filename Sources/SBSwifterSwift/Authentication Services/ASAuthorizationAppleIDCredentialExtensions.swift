52856e871f7758a94cbd3935d8c1e1a90ff18b2f
Scott Brenner <brenner.scott.980@icloud.com>
2020-05-25 09:27:38 +0900

    better description

---
+++ Sources/SBSwifterSwift/Authentication Services/ASAuthorizationAppleIDCredentialExtensions.swift
@@ -0,0 +1,36 @@
+import AuthenticationServices
+
+extension ASAuthorizationAppleIDCredential {
+    
+    private struct Printable: Encodable, CustomStringConvertible {
+        
+        let identityToken: String
+        
+        let authorizationCode: String
+        
+        let user: String
+        
+        let name: String?
+        
+        let email: String?
+        
+        init?(credential: ASAuthorizationAppleIDCredential) {
+            
+            guard let identityToken = credential.identityToken?.string(),
+                let authorizationCode = credential.authorizationCode?.string() else {
+                    return nil
+            }
+            self.identityToken = identityToken
+            self.authorizationCode = authorizationCode
+            user = credential.user
+            name = [credential.fullName?.givenName, credential.fullName?.familyName]
+                .compactMap { $0 }
+                .joined(separator: " ")
+            email = credential.email
+        }
+    }
+    
+    open override var description: String {
+        Printable(credential: self)!.description
+    }
+}
