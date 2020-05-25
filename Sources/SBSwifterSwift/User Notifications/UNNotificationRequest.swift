9f6bfdd7920445241196f5d2a2b655deb93cd629
Scott Brenner <brenner.scott.980@icloud.com>
2020-05-25 16:57:41 +0900

    Initial commit

---
+++ Sources/SBSwifterSwift/User Notifications/UNNotificationRequest.swift
@@ -0,0 +1,31 @@
+import UserNotifications
+
+public extension UNNotificationRequest {
+    
+    func add(to center: UNUserNotificationCenter = .current()) -> AnyPublisher<Void, UNError> {
+        
+        Future<Void, UNError> { promise in
+            center.add(self) { error in
+                if let error = error as? UNError {
+                    promise(.failure(error))
+                }
+                else {
+                    promise(.success(()))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+
+public extension Sequence where Element == UNNotificationRequest {
+    
+    func add(to center: UNUserNotificationCenter = .current()) -> AnyPublisher<Void, UNError> {
+        
+        Publishers
+            .Sequence<[AnyPublisher<Void, UNError>], UNError>(sequence: map { $0.add(to: center) })
+            .flatMap { $0 }
+            .eraseToAnyPublisher()
+    }
+}
