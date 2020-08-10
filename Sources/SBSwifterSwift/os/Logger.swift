b6740da82d73d74289501aec17e31d9af4ec56e8
Scott Brenner <brenner.scott.980@icloud.com>
2020-08-10 12:38:58 -0700

    bug fix

--- Sources/SBSwifterSwift/os/Logger.swift
+++ Sources/SBSwifterSwift/os/Logger.swift
@@ -1,6 +1,6 @@
 import os
 
-@available(OSX 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
+@available(macOS 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
 public extension Logger {
     
     /// Creates a custom logger for logging to a subsystem and category where the subsystem is set to your app's bundle identifier.
