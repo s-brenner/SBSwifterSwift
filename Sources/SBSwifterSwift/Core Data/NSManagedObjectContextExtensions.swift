5e3a715efe3589639a8fbb49ad0e4a9a400c8ae1
Scott Brenner <brenner.scott.980@icloud.com>
2020-08-18 15:37:46 -0800

    enhanced logging

--- Sources/SBSwifterSwift/Core Data/NSManagedObjectContextExtensions.swift
+++ Sources/SBSwifterSwift/Core Data/NSManagedObjectContextExtensions.swift
@@ -76,7 +76,11 @@ extension NSManagedObjectContext.ChangesPublisher {
         
         private var fetchedResultsController: NSFetchedResultsController<Object>?
         
-        private var demand: Subscribers.Demand = .none
+        private var demand: Subscribers.Demand = .none {
+            didSet {
+                log("Demand is \(demand)")
+            }
+        }
         
         @available(macOS 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
         private lazy var logger = Logger(category: "NSManagedObjectContext.ChangesPublisher")
@@ -93,12 +97,16 @@ extension NSManagedObjectContext.ChangesPublisher {
             fetchedResultsController!.delegate = self
             do {
                 try fetchedResultsController!.performFetch()
-                if #available(macOS 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, *) {
-                    logger.info("NSFetchedResultsController performed fetch")
-                }
+                log("NSFetchedResultsController performed fetch")
             } catch {
                 downstream.receive(completion: .failure(error))
+                log(error.localizedDescription, isError: true)
             }
+        }
+        
+        private func log(_ message: String, isError: Bool = false) {
+            guard #available(macOS 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, *) else { return }
+            isError ? logger.error("\(message)") : logger.info("\(message)")
         }
         
         func request(_ demand: Subscribers.Demand) {
@@ -107,14 +115,13 @@ extension NSManagedObjectContext.ChangesPublisher {
         }
         
         func cancel() {
+            log("Cancel")
             fetchedResultsController?.delegate = nil
             fetchedResultsController = nil
         }
         
         func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-            if #available(macOS 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, *) {
-                logger.info("NSFetchedResultsController did change content")
-            }
+            log("NSFetchedResultsController did change content")
             fulfillDemand()
         }
         
@@ -122,9 +129,7 @@ extension NSManagedObjectContext.ChangesPublisher {
             guard demand > 0 else { return }
             let objects = fetchedResultsController?.fetchedObjects ?? []
             let newDemand = downstream.receive(objects)
-            if #available(macOS 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, *) {
-                logger.info("\(objects.count) objects sent downstream")
-            }
+            log("\(objects.count) \(objects.count == 1 ? "object" : "objects") sent downstream")
             demand += newDemand
             demand -= 1
         }
