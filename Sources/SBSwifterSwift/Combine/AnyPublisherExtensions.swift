4f99ce9c24caa1ffe6bd5a022d738571467a1db0
Scott Brenner <brenner.scott.980@icloud.com>
2020-12-25 06:06:45 -0800

    SAB

---
+++ Sources/SBSwifterSwift/Combine/AnyPublisherExtensions.swift
@@ -0,0 +1,15 @@
+import Combine
+
+public extension AnyPublisher {
+    
+    static func just(_ output: Output) -> Self {
+        Just(output)
+            .setFailureType(to: Failure.self)
+            .eraseToAnyPublisher()
+    }
+        
+    static func fail(with error: Failure) -> Self {
+        Fail(error: error)
+            .eraseToAnyPublisher()
+    }
+}
--- Sources/SBSwifterSwift/Combine/PublisherExtensions.swift
+++ Sources/SBSwifterSwift/Combine/PublisherExtensions.swift
@@ -5,10 +5,52 @@ public extension Publisher where Self.Failure == Never {
     
     func assignWithoutRetain<Root: AnyObject>(
         to keyPath: ReferenceWritableKeyPath<Root, Self.Output>,
-        on object: Root) -> AnyCancellable {
+        on object: Root
+    ) -> AnyCancellable {
         
         sink { [weak object] value in
             object?[keyPath: keyPath] = value
         }
     }
 }
+
+public extension Publisher {
+    
+    func convertToResult() -> AnyPublisher<Result<Output, Failure>, Never> {
+        self.map(Result.success)
+            .catch { Just(.failure($0)) }
+            .eraseToAnyPublisher()
+    }
+    
+    func unwrap<T>(
+        orThrow error: @escaping @autoclosure () -> Failure
+    ) -> Publishers.TryMap<Self, T> where Output == Optional<T> {
+        tryMap { output in
+            switch output {
+            case .some(let value):
+                return value
+            case nil:
+                throw error()
+            }
+        }
+    }
+    
+    func validate(
+        using validator: @escaping (Output) throws -> Void
+    ) -> Publishers.TryMap<Self, Output> {
+        tryMap { output in
+            try validator(output)
+            return output
+        }
+    }
+}
+
+//public extension Publisher where Output == Data {
+//
+//    func decode<T: Decodable>(
+//        as type: T.Type = T.self,
+//        using decoder: JSONDecoder = .init()
+//    ) -> Publishers.Decode<Self, T, JSONDecoder> {
+//        decode(type: type, decoder: decoder)
+//    }
+//}
