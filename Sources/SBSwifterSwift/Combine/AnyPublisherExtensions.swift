18cc1535e8d39ca2dbd1cb57d3fae9f55f423884
Scott Brenner <brenner.scott.980@icloud.com>
2022-09-27 20:05:19 -0600

    SAB

--- Sources/SBSwifterSwift/Combine/AnyPublisherExtensions.swift
+++ Sources/SBSwifterSwift/Combine/AnyPublisherExtensions.swift
@@ -1,17 +1,34 @@
 #if canImport(Combine)
 import Combine
 
-public extension AnyPublisher {
+extension AnyPublisher {
     
-    static func just(_ output: Output) -> Self {
+    public static func just(_ output: Output) -> Self {
         Just(output)
             .setFailureType(to: Failure.self)
             .eraseToAnyPublisher()
     }
         
-    static func fail(with error: Failure) -> Self {
+    public static func fail(with error: Failure) -> Self {
         Fail(error: error)
             .eraseToAnyPublisher()
+    }
+    
+    public func async() async throws -> Output {
+        try await withCheckedThrowingContinuation { continuation in
+            var cancellable: AnyCancellable?
+            cancellable = first().sink { result in
+                switch result {
+                case .finished:
+                    break
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+                cancellable?.cancel()
+            } receiveValue: { value in
+                continuation.resume(with: .success(value))
+            }
+        }
     }
 }
 #endif
